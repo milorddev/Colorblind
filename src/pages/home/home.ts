27b6934a90a3b49c4fa3baff5ad3a07ea4e1365a
sciencefreak500 <immunitygame@gmail.com>
2018-04-15 02:40:05 +0000

    canvas steps 1

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,8 +7,37 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	@ViewChild('imgCaptured') imgCaptured:any;
+	@ViewChild('myCanvas') canvasRef:any;
 
+	img:any;
+	canvas:any;
+	coords:any = {
+		x:0,
+		y:0
+	};
+  constructor(public navCtrl: NavController) {
+  	
   }
 
+  tapEvent(event){
+  	console.log("event",event);
+  	this.img = this.imgCaptured.nativeElement;
+  	this.canvas = this.canvasRef.nativeElement;
+  	this.canvas.width = this.img.width;
+  	this.canvas.height = this.img.height;
+  	this.canvas.getContext('2d').drawImage(this.img,0,0,this.img.width, this.img.height);
+
+  	var pixelData = this.canvas.getContext('2d').getImageData();
+  	console.log("pixelData",pixelData);
+  }
+
+
+// var img = document.getElementById('my-image');
+// var canvas = document.createElement('canvas');
+// canvas.width = img.width;
+// canvas.height = img.height;
+// canvas.getContext('2d').drawImage(img, 0, 0, img.width, img.height);
+
+// var pixelData = canvas.getContext('2d').getImageData(event.offsetX, event.offsetY, 1, 1).data;
 }
